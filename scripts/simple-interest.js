eedaf69eab0ba12ba5fb6de0bc2e5c9da34ff32e
Darry Diaz <darry.a.diaz@gmail.com>
2025-06-20 09:03:33 +0800

    finalized UI for formulas

--- scripts/simple-interest.js
+++ scripts/simple-interest.js
@@ -12,6 +12,7 @@ function calculateSI() {
     const SI = (P * R * T) / 100;
 
     const explanation = `
+        <div class="given"><h3> Given: P = ${P}, R = ${R}, T = ${T}</h2></div>
         <div class="step">Step 1: Use the formula <strong>SI = (P × R × T) / 100</strong></div>
         <div class="step">Step 2: Substitute the values: SI = (${P} × ${R} × ${T}) / 100</div>
         <div class="step">Step 3: Multiply: ${P} × ${R} × ${T} = ${P * R * T}</div>

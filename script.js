391f2b34ce03ffa631939e41e36935437e615288
Darry Diaz <darry.a.diaz@gmail.com>
2025-06-20 15:48:02 +0800

    separated js script  in index

---
+++ script.js
@@ -0,0 +1,13 @@
+    const collapsibles = document.querySelectorAll(".collapsible");
+    collapsibles.forEach(btn => {
+        btn.addEventListener("click", function () {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+        });
+    });
